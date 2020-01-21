5fb7fb34c45219efe41157d90892135351e63260
Bernard (yoga) Tatin <bernard.tatin@outlook.fr>
2020-01-21 10:28:41 +0100

    The greatest part is done!
    - print the hexa part
    - TOTO: the ASCII part

--- dhex/dhex.d
+++ dhex/dhex.d
@@ -8,7 +8,22 @@ module dhexd;
 import std.stdio,
 		std.file;
 
-string app_version = "0.1.0";
+string app_version = "0.1.1";
+int chunck_size = 16;
+
+void on_file(string file_name) {
+	auto f = File(file_name, "r");
+	int address = 0;
+
+	foreach (ubyte[] buffer; chunks(f, chunck_size)) {
+		writef ("%08x: ", address);
+		address += chunck_size;
+		foreach (b; buffer) {
+			writef ("%02x ", b);
+		}
+		writef("\n");
+	}
+}
 
 void main(string[] args) {
 	writefln("dhexd %s\n", app_version);
@@ -19,6 +34,7 @@ void main(string[] args) {
 			writefln ("-> ERROR: %s is not a file", a);
 		} else {
 			writefln ("-> SUCCESS: %s is a file!!!", a);
+			on_file (a);
 		}
 	}
 }
