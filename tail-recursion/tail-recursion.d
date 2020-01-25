782b667d5195be80f6e3747441c54a70ad813eeb
Bernard (yoga) Tatin <bernard.tatin@outlook.fr>
2020-01-25 15:20:28 +0100

    first test of tail recursion with Dmd

---
+++ tail-recursion/tail-recursion.d
@@ -0,0 +1,25 @@
+/*
+ * tail-recursion.d
+ * testing tail-recursion in D
+ */
+
+module tail_recursion;
+
+import std.stdio: writef, writefln;
+
+auto tail_recursion(immutable int n) {
+	if (n % 1000 == 0) {
+		writefln("%8d", n);
+	}
+	if (n < 0) {
+		return n;
+	} else if (n > 100000) {
+		return n;
+	} else {
+		return tail_recursion(n + 1);
+	}
+}
+
+void main() {
+	writefln("--> %d", tail_recursion(0));
+}
