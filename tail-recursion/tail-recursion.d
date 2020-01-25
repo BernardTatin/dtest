2c6cc6298769f251d1f084ec7b5889de47f97548
Bernard (yoga) Tatin <bernard.tatin@outlook.fr>
2020-01-25 17:30:35 +0100

    clean code

--- tail-recursion/tail-recursion.d
+++ tail-recursion/tail-recursion.d
@@ -2,6 +2,10 @@
  * tail-recursion.d
  * testing tail-recursion in D
  *
+ * usage:
+ 	$ dmd|ldc2 tail-recursion.d -of=tail-recursion.exe \
+ 		&& ./tail-recursion.exe
+
  * first attempt: no tail recursion in dmc and ldc2
  */
 
@@ -9,16 +13,9 @@ module tail_recursion;
 
 import std.stdio: writef, writefln;
 
-pure auto tail_recursion(immutable int n) {
-	/*
-	if (n % 1000 == 0) {
-		writefln("%8d", n);
-	}
-	*/
+pure auto tail_recursion(immutable(int) n) {
 	if (n < 0) {
 		return n;
-	/* } else if (n > 100000) {
-		return n; */
 	} else {
 		return tail_recursion(n + 1);
 	}
